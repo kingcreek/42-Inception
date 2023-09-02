2cd4d35a458373154b2a95dbfbd4637214446cef
kingcreek <israel_launion@hotmail.com>
2023-09-02 04:33:59 +0200

    add hosts

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -5,15 +5,6 @@
 #  \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ 
 
 # *************************************************************************** #
-
-# Check if SO is ubuntu
-if [[ $(cat /etc/os-release | grep "Ubuntu") ]]; then
-    # add routes to /etc/hosts
-    echo "127.0.0.1 imurugar.42.fr" >> /etc/hosts
-    echo "127.0.0.1 www.imurugar.42.fr" >> /etc/hosts
-    echo "127.0.0.1 https://www.imurugar.42.fr" >> /etc/hosts
-fi
-
 apk add --no-cache nginx openssl
 mkdir -p /etc/nginx/ssl
 openssl req -x509 -new -newkey rsa:4096 -nodes -keyout /etc/nginx/ssl/imurugar.key -out /etc/nginx/ssl/imurugar.crt -subj "/C=ES/ST=/L=/O=/OU=/CN=imurugar.42.fr"
